9f8f1b1be626ff2da768f58783a2e0503f52e911
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-10-09 18:37:56 -0500

    Use spans instead of indices
    This allows you to have patterns more than one character long
    Also updated tests

--- embedded_substring.py
+++ embedded_substring.py
@@ -1,31 +1,87 @@
 import sys
 
-from termcolor import cprint
+import termcolor
 
 
-def match(target, substring):
-    indices = []
-    current_sub = 0
-    sub_length = len(substring)
-    for i, char in enumerate(target):
-        if current_sub >= sub_length:
+def match(target, patterns):
+    """
+    :param patterns: the patterns to search for within the target string
+    :type patterns: list
+    :param target: the string to search within
+    :type target: str
+    """
+    spans = []
+    current_index = 0
+    for pattern in patterns:
+        i = target.find(pattern, current_index)
+
+        if i < 0:
             break
-        if char == substring[current_sub]:
-            indices.append(i)
-            current_sub += 1
-    if len(indices) == sub_length:
-        return indices
+        else:
+            end = i + len(pattern)
+            spans.append(range(i, end))
+            current_index = end
+
+    if len(spans) == len(patterns):
+        return spans
     else:
         return None
 
 
-if __name__ == '__main__':
-    input_str = str(sys.stdin.read())
-    find_str = ''.join(sys.argv[1:])
-    matches = match(input_str, find_str)
+def main(target, patterns):
+    """
+    This method returns the result of the complete process. It can be useful if used from the python console or in a
+    library
+    :param target: The string to search inside
+    :type target: str
+    :param patterns: The substring to search for
+    :type patterns: list
+    :return: The string with the substring bolded highlighted in red
+    """
+    input_len = len(target)
 
-    for i, char in enumerate(input_str):
-        if i in matches:
-            cprint(char, color='red', attrs=['bold'], end='')
+    matches = match(target, patterns)
+    if matches is None:
+        print('No complete matches found', file=sys.stderr)
+        return
+
+    string_builder = []
+    current_end = 0
+
+    def colored(s):
+        """
+        wrapper around termcolor.colored, with program default colors and attributes
+        :param s: the string to color
+        :type s: str
+        :rtype: str
+        """
+        return termcolor.colored(s, color='red', attrs=['bold'])
+
+    for m in matches:
+        if m.start == 0 or m.start == current_end:
+            string_builder.append(colored(target[m.start:m.stop]))
+            current_end = m.stop
         else:
-            print(char, end='')
+            string_builder.append(target[current_end:m.start])
+            string_builder.append(colored(target[m.start:m.stop]))
+            current_end = m.stop
+
+    if current_end != input_len:
+        string_builder.append(target[current_end:])
+
+    return ''.join(string_builder)
+
+
+if __name__ == '__main__':
+    queries = sys.argv[1:]
+    if len(queries) == 1:
+        queries = [char for char in queries[0]]
+        # Strip of placeholder characters
+        while True:
+            try:
+                queries.remove('_')
+            except ValueError as _:
+                break
+
+    result = main(str(sys.stdin.read()), queries)
+    print(result)
--- test_embedded_substring.py
+++ test_embedded_substring.py
@@ -4,12 +4,16 @@ from embedded_substring import match
 
 
 class TestEmbeddedString(unittest.TestCase):
+    @staticmethod
+    def list_from_substring(s):
+        return [char for char in s]
+
     def test_basic(self):
         target = "filler filler filler a filler filler filler b filler filler filler c filler"
         substring = "abc"
 
-        expected = [21, 44, 67]
-        actual = match(target, substring)
+        expected = [range(21, 22), range(44, 45), range(67, 68)]
+        actual = match(target, self.list_from_substring(substring))
         self.assertIsNotNone(actual)
         self.assertEqual(expected, actual)
 
@@ -17,8 +21,8 @@ class TestEmbeddedString(unittest.TestCase):
         target = "a filler filler filler b filler filler filler c filler filler filler d filler"
         substring = "abcd"
 
-        expected = [0, 23, 46, 69]
-        actual = match(target, substring)
+        expected = [range(0, 1), range(23, 24), range(46, 47), range(69, 70)]
+        actual = match(target, self.list_from_substring(substring))
         self.assertIsNotNone(actual)
         self.assertEqual(expected, actual)
 
@@ -26,15 +30,31 @@ class TestEmbeddedString(unittest.TestCase):
         target = "filler filler filler filler filler filler filler filler filler filler"
         substring = "abcd"
 
-        actual = match(target, substring)
+        actual = match(target, self.list_from_substring(substring))
         self.assertIsNone(actual)
 
     def test_none_incomplete(self):
         target = "filler filler filler a filler filler filler b filler filler filler c filler"
         substring = "abcd"
 
-        actual = match(target, substring)
+        actual = match(target, self.list_from_substring(substring))
         self.assertIsNone(actual)
+
+    def test_multi_character_patterns(self):
+        target = "this definitely is some cool filler text"
+        patterns = ["this", "is", "cool"]
+
+        expected = [range(0, 4), range(16, 18), range(24, 28)]
+        actual = match(target, patterns)
+        self.assertEqual(expected, actual)
+
+    def test_one_match(self):
+        target = "test"
+        patterns = ["test"]
+
+        expected = [range(0, 4)]
+        actual = match(target, patterns)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
